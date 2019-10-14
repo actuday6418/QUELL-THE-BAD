b4ff1534b3f21582bd79fdcefba5c8f618e58693
actuday6418 <56124831+actuday6418@users.noreply.github.com>
2019-10-14 12:51:36 +0000

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
-#include<stdlib.h>
+#include<cstdlib>
 #include<list>
+#include<random>
 
 using namespace std;
 
+default_random_engine engine;
 
 class space
 	{
@@ -17,7 +19,7 @@ class space
 				for(int j=0;j<100;j++)
 					map[i][j]=false;
             for(int i=0;i<10000;i++)
-	      	        lightmap[i]=40;
+	      	        lightmap[i]=70;
             for(int i=0;i<10000;i++)
 	      	        temperaturemap[i]=40;
 		}
@@ -46,15 +48,16 @@ class organism
 	public:
 		static int ID;
 	        int thisID;
+		int parentID;
 		int mapposition;
 		int preferlight;
 		int health;
 		int LifeTicks;
 		int prefertemperature;
-        organism();
-        organism(organism*);
-		int turn();
-		int mutate();
+       		organism();
+	        organism(organism*);
+		void turn();
+		void mutate(int);
 		void FindMyHealth();
 		void move();
 		void show();
@@ -70,7 +73,8 @@ organism::organism()
 		ID++;
 		mapposition=177;
 		LifeTicks=0;
-      	int posx = mapposition/100;
+		parentID=-1;
+        	int posx = mapposition/100;
 		int posy = mapposition%100;
 		Space.map[posx][posy]=true;
 		preferlight=50;
@@ -78,25 +82,9 @@ organism::organism()
 		health=100;
 		}
 
-organism::organism(organism* parent)
-     		{
-		thisID=ID;
-		ID++;
-		mapposition=177;
-		LifeTicks=0;
-		int posx = parent->mapposition/100;
-		int posy = parent->mapposition%100;
-		Space.map[posx][posy]=true;
-		preferlight=parent->preferlight;
-		prefertemperature=parent->prefertemperature;
-		preferlight+=rand()%3-1;
-		prefertemperature+=rand()%3-1;
-		health=100;
-		}
-
 void organism::FindMyHealth()
 	{
-		health=100-LifeTicks-5*abs(prefertemperature-Space.temperaturemap[mapposition])-5*abs(preferlight-Space.lightmap[mapposition]);
+		health=100-LifeTicks-abs(prefertemperature-Space.temperaturemap[mapposition])-abs(preferlight-Space.lightmap[mapposition]);
 		if(health<=0)
 			health=-1;
 	}	
@@ -104,39 +92,38 @@ void organism::FindMyHealth()
 
 void organism::show()
 {
-	cout<<"\n[$]ID"<<ID<<" [$]Temp"<<prefertemperature<<" [$]Light"<<preferlight<<" [$]Health"<<health<<" [$]Map_Position"<<mapposition<<"\n";
+	cout<<"\n[$]ID"<<thisID<<"  [$]Parent ID"<<parentID<<" [$]Life_Ticks"<<LifeTicks<<" [$]Temp"<<prefertemperature<<" [$]Light"<<preferlight<<" [$]Health"<<health<<" [$]Map_Position"<<mapposition<<"\n";
 }
 
-	
-int organism::turn()
+	void organism::turn()
 	{   
 		move();
-		LifeTicks++;
+		LifeTicks++;	
 		FindMyHealth();
-		if(rand()%20==7)
-			{
-				if(mutate())
-				return 1;
-			}
-		return 0;
+		int u=20;
+		uniform_int_distribution <int> dist(0,u);
+		{
+			if(dist(engine)==0)
+			{	mutate(0);}
+		}
 	}
-
 
 
 void organism::move()
 	{
 		int BackUpMapPosition=mapposition;
 		int posx = mapposition/100;
-		int posy = mapposition%100;
+ 		int posy = mapposition%100;
 		Space.map[posx][posy]=false;
 		int i=0,x;
-
+		uniform_int_distribution <int> distro(0,100);
+		
 		do
 		 {
-			x=rand()%101;
-			if(mapposition > 9900)
+			x=distro(engine);
+ 			if(mapposition > 9900)
 				x=2;
-			else if(mapposition < 100)
+ 			else if(mapposition < 100)
 				x=37;
 			i++;
 		if(x>90)
@@ -154,10 +141,10 @@ void organism::move()
 			mapposition-=100;
 		if(i==10)
 			break;
-       		 posx = mapposition/100;
+       		posx = mapposition/100;
 		posy = mapposition%100;
 		}
-        while(mapposition==177||Space.map[posx][posy]==true);
+        while(Space.map[posx][posy]==true);
         
 		if(i!=10)
 			{
@@ -175,18 +162,107 @@ void organism::move()
 
 	}
 
-	
-int organism::mutate()	
+	void organism::mutate(int def)	
 	{
-		if(Space.map[1][76]==false)
-		{
 		organism x;
-                x=this;
-		life.push_front(x);
-		return 1;
+		int y=x.thisID;
+                x=*this;
+		x.thisID=y;
+		x.health=100;
+		x.parentID=thisID;
+                x.LifeTicks = 0; 
+		uniform_int_distribution <int> di(-1,1),undi(1,2);
+		y=undi(engine);
+		if(y==1)
+			x.preferlight += 2*di(engine);
+		else if(y==2)
+			x.prefertemperature +=2*di(engine);
+		int posx = mapposition/100;
+		int posy = mapposition%100;
+		bool Flag=false;
+		y=0;
+		uniform_int_distribution <int> distro(0,7);
+		while(y<10&&!Flag)
+		{
+		int Random=distro(engine);
+			if(Random==0)
+			{	if(Space.map[posx+1][posy]==false)
+				{
+					posx+=1;
+					Flag = true;
+				}
+			}	
+				else if(Random==1)
+				{	if(Space.map[posx-1][posy]==false)
+				{
+					posx-=1;
+					Flag=true;
+				}}
+				
+				else if(Random==2)
+				{	if(Space.map[posx][posy+1]==false)
+				{
+					posy+=1;
+					Flag=true;
+				}}
+			
+				
+				else if(Random==3)
+				{if(Space.map[posx][posy-1]==false)
+				{
+					posy-=1;
+					Flag=true;
+				}}
+				
+				
+				else if(Random==4)
+				{if(Space.map[posx+1][posy+1]==false)
+				{
+					posy+=1;
+					posx+=1;
+					Flag=true;
+				}}
+					
+				else if(Random==5)
+				{	if(Space.map[posx-1][posy-1]==false)
+					{
+						posy-=1;
+						posx-=1;
+						Flag=true;
+					}}
+					
+					else if(Random==6)
+					{if(Space.map[posx+1][posy-1]==false)
+					{
+						posy-=1;
+						posx+=1;
+						Flag=true;
+					}}
+					
+			                else if(Random==7)
+					{if(Space.map[posx-1][posy+1]==false)
+					{
+						posy+=1;
+						posx-=1;
+						Flag=true;
+					}}
+					
+			y++;
+			x.mapposition = posx*100+posy;
+			if(x.mapposition>10000||x.mapposition<0)
+				Flag=false;
 		}
-		else 
-			return 0;
+		if(Flag)
+		{
+		Space.map[posx][posy]=true;
+		life.push_front(x);
+
+		
+		}
+		else
+		{
+			organism::ID-=1;
+		}
 	}
 
 
@@ -197,44 +273,57 @@ int Begin()
 	    
             organism m;
             life.push_front(m); 
-	    int y;
+	    int y=0,z;
 	    list <organism>::iterator x;
-		unsigned long int Ticks=0;
-        x= life.begin();
-	list <organism> ::iterator t;
-		while(Ticks<1000)
+	    unsigned long int Ticks=0;
+            x= life.begin();
+  	    list <organism> ::iterator t;
+
+	    while(Ticks<1000)
 		{
 			Ticks++;
-	    if(life.empty())
-	    {
-		    return 7;
-	    }
-            if(x==life.end())
-                x=life.begin();
-            if(x->health==-1)
-                {
-                 x=life.erase(x);
-                continue;
-                }
-	    x->turn();
-		t = life.begin();
-                while(t!=life.end())
-                {
-		  cout<<"Iterations "<<Ticks<<"   organisms alive  "<<life.size()<<"  ";
-        	  t->show();
-   		     ++t;
-		}
-		++x;
-		}
+		//	cin>>y;
+	    		if(life.empty())
+	   			 {
+					cout<<"Terminated on drain";
+		  			  return 7;
+	   			 }
+           		 
+            			 x=life.begin();
+				 while(x!=life.end())
+				 {
+           			 if(x->health==-1)
+               			 {
+              				   x=life.erase(x);
+              				   continue;
+               			 }
+				 x->turn();
+				 }
+		       	 t = life.begin();
+			 z=y=0;
+               		 while(t!=life.end())
+               		 {	
+			 y+=t->prefertemperature;	 
+			 z+=t->preferlight;
+				 {
+			//	  cout<<"Iterations "<<Ticks<<"   organisms alive  "<<life.size()<<"  ";
+        			  t->show();
+				 }
+   		    		  ++t;
+			 }
+
+			if(life.size())
+		 cout<<"Iterations "<<Ticks<<"  PT"<<y/life.size()<<"  PL"<<z/life.size()<<" Size"<<life.size()<<"\n";
+			 }	
 		return 0;
 	}		
 
+
+
 int main()
 {
-srand((unsigned)time(0));
-while(true)
-{cout<<"&";	if(Begin()!=7)
-		break;}
+	engine.seed(time(0));
+	cout<<"&";Begin();
 cout<<"END!\n";
 }
 
